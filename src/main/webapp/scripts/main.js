33b0d065273b0718fee1ae1dc33ae6e0977fe573
TarminaRX <tarminarobes@hotmail.com>
2025-02-23 23:33:01 +0800

    fix back and forward.

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -4,6 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const strPath = window.location.pathname;
   const pathSplitted = strPath.split("/").filter(part => part !== '');
   const locationUrl = pathSplitted[pathSplitted.length - 1];
+
+    /** @param Event event */
+  window.onpopstate = (event) => {
+    if (event.state) {
+      window.location.reload(window.location.pathname);
+    }
+  }
   
   refreshPage(locationUrl);
 });
