539e6ddb485441cd9d69a1ea8a09a7151b936b17
Navidad12 <elijahgabriel7104@gmail.com>
2025-02-27 19:23:45 +0800

    AdminCreateServlet done? Haven't tested, will finish later or tomorrow

---
+++ src/main/java/net/rnzonly/mtwo/controllers/AdminCreateServlet.java
@@ -0,0 +1,65 @@
+package net.rnzonly.mtwo.controllers;
+
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import net.rnzonly.mtwo.models.DataAccess;
+import net.rnzonly.mtwo.models.ErrorFolio;
+import net.rnzonly.mtwo.models.UserFolio;
+import net.rnzonly.mtwo.models.UserMessage;
+import net.rnzonly.mtwo.utilities.JsonConverter;
+
+@WebServlet("/api/create")
+class AdminCreateServlet extends TemplateServlet {
+  private DataAccess da = new DataAccess();
+
+  @Override
+  protected void processRequest(HttpServletRequest request,
+                                HttpServletResponse response) throws Exception {
+
+    /* PLEASE FOLLOW FORMAT */
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    PrintWriter aba = response.getWriter();
+    ErrorFolio messageError = null;
+
+    HttpSession sq = request.getSession(true);
+    if (sq.getAttribute("currentUser") == null) {
+      messageError = new ErrorFolio(true, "Session expired, Log in again!");
+      aba.print(JsonConverter.convertToJson(messageError));
+      return;
+    }
+
+    UserFolio currUser = (UserFolio)sq.getAttribute("currentUser");
+    
+    if(!"admin".equalsIgnoreCase(currUser.user_role()) && !"super_admin".equalsIgnoreCase(currUser.user_role())){
+      messageError = new ErrorFolio(true, "You are not authorized to do this!");
+      aba.print(JsonConverter.convertToJson(messageError));
+      return;
+    }
+
+    String uNametoCreate = request.getParameter("user_name");
+    String password = request.getParameter("password");
+
+    if(da.checkIfUserExists(uNametoCreate)){
+        messageError = new ErrorFolio(true, "User already exists!");
+        aba.print(JsonConverter.convertToJson(messageError));
+        return;
+      }else{
+        messageError = da.registerUser(uNametoCreate, password, "user");
+        if(messageError.isError() == false){
+            messageError = new ErrorFolio(false, "User created successfully!");
+        }else{
+            messageError = new ErrorFolio(true, "User creation failed. User may already exist");
+        }
+      }
+
+    
+    
+    aba.print(JsonConverter.convertToJson(messageError));
+
+    // aba.print(JsonConverter.convertToJson(rm));
+  }
+}
