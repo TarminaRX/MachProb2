9690b64e41f9b91780794f82faae80411bbbaadb
TarminaRX <tarminarobes@hotmail.com>
2025-03-01 15:25:29 +0800

    remove testing features.

--- src/main/webapp/scripts/websocket.js
+++ src/main/webapp/scripts/websocket.js
@@ -1,27 +1,27 @@
-var SetupHotReload = () => {
-  const socket = new WebSocket(`ws://localhost:8080/WebReload/`);
-  
-  socket.addEventListener('message', (event) => {
-    try {
-      const data = JSON.parse(event.data);
-      
-      if (data.type === 'reload') {
-          location.reload();
-      }
-    } catch (error) {
-      console.error('Error processing WebSocket message:', error);
-    }
-  });
-  
-  socket.addEventListener('close', () => {
-    console.log('WebSocket connection closed. Attempting to reconnect...');
-    setTimeout(SetupHotReload, 1000);
-  });
-  socket.addEventListener('error', (error) => {
-    console.error('WebSocket error:', error);
-  });
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    SetupHotReload();
-});
+//var SetupHotReload = () => {
+//  const socket = new WebSocket(`ws://localhost:8080/WebReload/`);
+//
+//  socket.addEventListener('message', (event) => {
+//    try {
+//      const data = JSON.parse(event.data);
+//
+//      if (data.type === 'reload') {
+//          location.reload();
+//      }
+//    } catch (error) {
+//      console.error('Error processing WebSocket message:', error);
+//    }
+//  });
+//
+//  socket.addEventListener('close', () => {
+//    console.log('WebSocket connection closed. Attempting to reconnect...');
+//    setTimeout(SetupHotReload, 1000);
+//  });
+//  socket.addEventListener('error', (error) => {
+//    console.error('WebSocket error:', error);
+//  });
+//};
+//
+//document.addEventListener('DOMContentLoaded', () => {
+//    SetupHotReload();
+//});
